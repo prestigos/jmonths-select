0db1cad5f0e64d23368aade6be56751e83feb697
Christopher Luna <dev@christopher-luna.com>
2015-07-16 00:27:02 -0500

    Updated to the latest changes

--- jmonths-select.jquery.js
+++ jmonths-select.jquery.js
@@ -1,61 +1,91 @@
 (function($){
-$.fn.months_selector = function(options){
-    var $this = this;
+    $.fn.months_selector = function(options, arg){
+        var $this = this;
 
-    // Default settings
-    var settings = $.extend({
-        months: new Date().getMonth() + 1,
-        start_year: new Date().getFullYear(),
-        start_month: new Date().getMonth() + 1,
-        field: 'months'
-    }, options);
+        var update_selection = function($field){
+            var months = [];
+            $this.find('.month.selected').each(function(){
+                months.push($(this).data('month'));
+            });
+            $field.val(months.join(','));
+            return months;
+        };
 
-    // Hidden field to store the list of months
-    var $field = $("<input type='hidden' name='" + settings.field + "'/>");
-    $this.append($field);
+        if(options && typeof(options) == 'object'){
+            // Default settings
+            var settings = $.extend({
+                months: new Date().getMonth() + 1,
+                start_year: new Date().getFullYear(),
+                start_month: new Date().getMonth() + 1,
+                field: 'months',
+                special: []
+            }, options);
 
-    // Widget generation
-    var months = [];
-    var year = settings.start_year;
-    var month = settings.start_month;
-    for(var m=settings.months; m>0; --m){
-        months.push(
-            "<li class='month' data-month='" +
-            year + "-" + month +
-            "'>" + ('0' + month).slice(-2) + "</li>"
-        );
-        if(month == 1 || m == 1){
-            $this.append(
-                "<ul class='year'><li class='year-label'>" + year + "</li>" + months.join('') + "</ul>"
-            );
-            months = [];
-            month = 12;
-            year -= 1
+            // Hidden field to store the list of months
+            var $field = $("<input type='hidden' name='" + settings.field + "'/>");
+            $this.append($field);
+
+            // Widget generation
+            var months = [];
+            var year = settings.start_year;
+            var month = settings.start_month;
+            var cls = 'month';
+            for(var m=settings.months; m>0; --m){
+                if(settings.special.indexOf(year + '-' + month) >= 0){
+                    cls = 'month special'
+                }
+                else{
+                    cls = 'month'
+                }
+                months.push(
+                    "<li class='" + cls + "' data-month='" + year + "-" + ('0' + month).slice(-2) + "'>" +
+                    ('0' + month).slice(-2) +
+                    "</li>"
+                );
+                if(month == 1 || m == 1){
+                    $this.append(
+                        "<ul class='year'><li class='year-label'>" + year + "</li>" + months.join('') + "</ul>"
+                    );
+                    months = [];
+                    month = 12;
+                    year -= 1
+                }
+                else{
+                    month--;
+                }
+            }
+
+            var active = '';
+
+            $this.find('.month').click(function(){
+                $(this).toggleClass('selected');
+                update_selection($field);
+                $field.change();
+            });
+
+            $this.find('.year-label').click(function(){
+                $(this).siblings().each(function(){
+                    $(this).toggleClass('selected');
+                });
+                update_selection($field);
+                $field.change();
+            })
+            return $this;
         }
-        else{
-            month--;
+        else if(options && typeof(options) == 'string'){
+            if(options === 'setSelection'){
+                $this.find('.month').each(function(){
+                    var $month = $(this);
+                    var month = $month.data('month');
+                    if(arg.indexOf(month) >= 0){
+                        $month.addClass('selected');
+                    }
+                    else{
+                        $month.removeClass('selected');
+                    }
+                });
+                return update_selection($this.find('input:hidden'));
+            }
         }
     }
-
-    var active = '';
-
-    var update_selection = function(){
-        var months = [];
-        $this.find('.month.selected').each(function(){
-            months.push($(this).data('month'));
-        });
-        $field.val(months.join(':'));
-    };
-
-    $this.find('.month').click(function(){
-        $(this).toggleClass('selected');
-        update_selection();
-    });
-
-    $this.find('.year-label').click(function(){
-        $(this).siblings().each(function(){
-            $(this).toggleClass('selected');
-        });
-    })
-}
 })(jQuery);
