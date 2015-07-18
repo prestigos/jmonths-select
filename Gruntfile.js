616875a4f1576932602127c9346204c3b7183793
Christopher Luna <dev@christopher-luna.com>
2015-07-17 19:42:08 -0500

    Using vulcanize and stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,15 +14,62 @@ module.exports = function (grunt) {
     },
     'uglify'  : {
       target : {
-        files : { 'dist/months-selector.min.js' : 'src/months-selector.js' }
+        files : { 'dist/months-selector.js' : 'src/months-selector.js' }
+      }
+    },
+    'connect': {
+      demo: {
+        options: {
+          open: true,
+          keepalive: true
+        }
+      }
+    },
+    'gh-pages': {
+      options: {
+        clone: 'bower_components/months-selector'
+      },
+      src: [
+        'bower_components/**/*',
+        '!bower_components/months-selector/**/*',
+        'demo/*', 'src/*', 'index.html'
+      ]
+    },
+    vulcanize: {
+      default: {
+        options: {
+          inline: true,
+          'strip-excludes' : false
+        },
+        files: {
+          'dist/months-selector.html' : 'dist/months-selector.html'
+        }
+      }
+    },
+    clean : [ 'dist/months-selector.js' ],
+    'replace': {
+      example: {
+        src: ['src/*'],
+        dest: 'dist/',
+        replacements: [{
+          from: 'bower_components',
+          to: '..'
+        }]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-jslint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-text-replace');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-vulcanize');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('lint',  ['jslint']);
-  grunt.registerTask('default',  ['jslint', 'uglify']);
+  grunt.registerTask('default',  ['jslint', 'replace', 'uglify', 'vulcanize', 'clean']);
+  grunt.registerTask('deploy', ['gh-pages']);
+  grunt.registerTask('server', ['connect']);
 
 };
